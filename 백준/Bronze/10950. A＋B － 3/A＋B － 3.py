1f9c43f6573f314feaa77139f17c052b1812b421
eyeol <42065198+eyeol@users.noreply.github.com>
2026-01-02 21:44:06 +0900

    [Bronze V] Title: A+B - 3, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    T = int(input())
+    for _ in range(T):
+        A, B = map(int, input().split())
+        print(A + B)
+
+
+if __name__ == "__main__":
+    solution()
