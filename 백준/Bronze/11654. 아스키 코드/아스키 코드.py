7f552b8ae049f5e74c221964e39bb2c00b518f55
eyeol <42065198+eyeol@users.noreply.github.com>
2026-01-03 14:43:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 110064 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def solution():
+    A = input().strip()
+    print(ord(A))
+
+
+if __name__ == "__main__":
+    solution()
