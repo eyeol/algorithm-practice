cbd47afbece1591cf7801da9ed7304f78f40c68d
eyeol <42065198+eyeol@users.noreply.github.com>
2026-01-27 17:57:17 +0900

    [Silver IV] Title: 괄호, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,46 @@
+import sys
+
+input = sys.stdin.readline
+
+# DFS의 Nesting intervals property를 이용하면 될듯
+# stack을 쓰자
+
+
+def solution():
+    T = int(input())
+
+
+    # 괄호 인풋을 그래프 탐색으로 생각하면 됨
+    def is_Valid(ps: str):
+        
+        stack = []
+
+        # ( ; White -> Gray
+        # ) ; Gray -> Black
+
+        # VPS를 위한 규칙
+        # White -> Gray 후에 Black으로 칠하면서 마무리해야 함
+        # White -> Gray 없이 )가 먼저 나오는 것
+        for ch in ps:
+            if ch == "(":
+                stack.append(ch)
+            else: # ch = ")"
+                if not stack: # 스택이 비어있는데 Black으로 칠하는 상황
+                    return False
+                else: # stack is not empty
+                    stack.pop()
+        
+        if stack: # 모든 인풋 받은 후에 스택에 잔여 원소가 있으면 not Valid
+            return False
+
+        return True
+
+    for _ in range(T):
+        ps = input().strip()
+        if is_Valid(ps):
+            print("YES")
+        else:
+            print("NO")
+
+if __name__ == "__main__":
+    solution()
