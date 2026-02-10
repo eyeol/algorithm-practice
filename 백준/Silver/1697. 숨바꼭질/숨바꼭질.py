cb759fe175c3f4e482d0fb029c8a53f98828f7ca
eyeol <42065198+eyeol@users.noreply.github.com>
2026-02-10 15:18:06 +0900

    [Silver I] Title: 숨바꼭질, Time: 112 ms, Memory: 115152 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,36 +1,38 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
-
 
 def solution():
     N, K = map(int, input().split())
-
     if N >= K:
-        print(N-K)
-    else: # N < K
-        # BFS로..?
-        time = [1000000]*(K*2+1) # 0부터 2K까지
-        time[N] = 0
-        
-        def bfs(time: list, s: int):
-            q = deque([s])
-            
-            while q:
-                u = q.popleft()
-                next = [u-1, u+1, 2*u]
-                for index in next:
-                    if 0 <= index <= 2*K:
-                        if time[u]+1 < time[index]:
-                            time[index] = time[u] + 1
-                            if index == K:
-                                print(time[index])
-                                return
-                            q.append(index)
-        
-        bfs(time, N)
+        print(N - K)
+        return
 
+    MAX = 100000  
+    dist = [-1] * (MAX + 1)
+    q = deque([N])
+    dist[N] = 0
+
+    while q:
+        x = q.popleft()
+        if x == K:
+            print(dist[x])
+            return
+
+        nx = x - 1
+        if nx >= 0 and dist[nx] == -1:
+            dist[nx] = dist[x] + 1
+            q.append(nx)
+
+        nx = x + 1
+        if nx <= MAX and dist[nx] == -1:
+            dist[nx] = dist[x] + 1
+            q.append(nx)
+
+        nx = x * 2
+        if nx <= MAX and dist[nx] == -1:
+            dist[nx] = dist[x] + 1
+            q.append(nx)
 
 if __name__ == "__main__":
     solution()
