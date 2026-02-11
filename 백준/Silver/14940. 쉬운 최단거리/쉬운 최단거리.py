ab225c585c68f4f0f83be4545cc4951cdd1423fa
eyeol <42065198+eyeol@users.noreply.github.com>
2026-02-11 14:26:08 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 204 ms, Memory: 119048 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,48 +1,35 @@
 import sys
 from collections import deque
 
-input= sys.stdin.readline
-
-DX = [1, -1, 0, 0]
-DY = [0, 0, 1, -1]
+input = sys.stdin.readline
+DX = (1, -1, 0, 0)
+DY = (0, 0, 1, -1)
 
 def solution():
     N, M = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
-    # 도달못한 위치 확인 위해 visited 필요
-    visited = [[0 for _ in range(M)] for _ in range(N)]
 
-    r = 0
-    c = 0
-    # 목표지점 확인
+    sr = sc = -1
     for i in range(N):
         for j in range(M):
-            if board[i][j] == 2:
-                r = i
-                c = j
+            if board[i][j] == 1:
+                board[i][j] = -1         
+            elif board[i][j] == 2:
+                sr, sc = i, j
 
-    q = deque([(r, c)])
-    board[r][c] = 0
-    visited[r][c] = 1
+    q = deque([(sr, sc)])
+    board[sr][sc] = 0                    
 
     while q:
-        x,y = q.popleft()
+        x, y = q.popleft()
         cur = board[x][y]
         for k in range(4):
             nx = x + DX[k]
             ny = y + DY[k]
-            # 범위 안에 있고 갈 수 있는 땅이면
-            if 0<=nx<N and 0<=ny<M and board[nx][ny]!=0:
-                if visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    board[nx][ny] = cur + 1
-                    q.append((nx, ny))
-    
-    for i in range(N):
-        for j in range(M):
-            if not visited[i][j] and board[i][j] != 0:
-                board[i][j] = -1
-    
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == -1:
+                board[nx][ny] = cur + 1
+                q.append((nx, ny))
+
     for row in board:
         print(*row)
 
