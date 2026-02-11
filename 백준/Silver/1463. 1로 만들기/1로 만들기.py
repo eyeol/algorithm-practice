3a9617ba573cfadc880f86e82c3e04fc61d2fead
eyeol <42065198+eyeol@users.noreply.github.com>
2026-02-12 05:19:31 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 116200 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -7,21 +7,32 @@ input = sys.stdin.readline
 def solution():
     N = int(input())
     
-    dp = [0] * (N + 1)
+    dp = [(N, 0)]
+    visited = [0]*(N+1)
 
-    for i in range(2, N+1):
-        v = dp[i-1] + 1
-        if i % 2 == 0:
-            t = dp[i//2] + 1
-            if t < v:
-                v = t
-        if i % 3 == 0:
-            t = dp[i//3] + 1
-            if t < v:
-                v = t
-        dp[i] = v
+    i = 0
+    while dp[i][0] > 1:
+        num = dp[i][0]
+        curr = dp[i][1]
+        b1 = num // 3
+        b2 = num // 2
+        b3 = num - 1
+
+        if num%3 == 0:
+            if not visited[b1]:
+                dp.append((num//3, curr+1))
+                visited[b1] = 1
+        if num%2 == 0:
+            if not visited[b2]:
+                dp.append((num//2, curr+1))
+                visited[b2] = 1
+        if not visited[b3]:
+            dp.append((num-1, curr+1))
+            visited[b3] = 1
+        
+        i += 1
     
-    print(dp[N])
+    print(dp[i][1])
 
 if __name__ == "__main__":
     solution()
