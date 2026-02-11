8af4b20ed0a1e2b0a5504218476cc0ccffa41763
eyeol <42065198+eyeol@users.noreply.github.com>
2026-02-11 17:39:20 +0900

    [Silver II] Title: 최소 힙, Time: 168 ms, Memory: 114204 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,22 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    N = int(input())
+    heap = []
+
+    for _ in range(N):
+        num = int(input())
+        if num == 0:
+            if heap:
+                print(heapq.heappop(heap))
+            else:
+                print(0)
+        else:
+            heapq.heappush(heap, num)
+
+if __name__ == "__main__":
+    solution()
