382c37e0e09248306197c417b899df873b6c1227
eyeol <42065198+eyeol@users.noreply.github.com>
2026-02-11 16:24:54 +0900

    [Silver II] Title: 색종이 만들기, Time: 120 ms, Memory: 111752 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,56 +1,37 @@
 import sys
-
-input= sys.stdin.readline
-
+input = sys.stdin.readline
 
 def solution():
     N = int(input())
     board = [list(map(int, input().split())) for _ in range(N)]
 
-    # determine to cut
-    def d2c(sx: int, sy: int, side: int):
-        color = board[sx][sy]
-        b = 0
-        w = 0
-
-        if side == 1:
-            if color: # 파랑색 종이
-                b += 1
-            else:     # 하얀색 종이
-                w += 1
-            return b, w
-        
-        cut = 0
-        for i in range(sx, sx+side):
-            for j in range(sy, sy+side):
-                # 대표 색과 다르면 자르기
+    def dc(r, c, n):
+        color = board[r][c]
+        same = True
+        for i in range(r, r+n):
+            for j in range(c, c+n):
                 if board[i][j] != color:
-                    cut = 1
-        
-        if not cut:
-            if color:
-                b += 1
-            else:
-                w += 1
-        else: # cut
-            side = side // 2
-            b1, w1 = d2c(sx, sy, side)
-            b2, w2 = d2c(sx, sy+side, side)
-            b3, w3 = d2c(sx+side, sy, side)
-            b4, w4 = d2c(sx+side, sy+side, side)
-            b = b1 + b2 + b3 + b4
-            w = w1 + w2 + w3 + w4
-        
-        return b, w
+                    same = False
+                    break
+            if not same:
+                break
 
-    sx = 0
-    sy = 0
-    side = N    
-    b, w = d2c(sx, sy, side)
+        if same:
+            if color == 1:
+                return 1, 0   # (blue, white)
+            else:
+                return 0, 1
+
+        half = n // 2
+        b1, w1 = dc(r, c, half)
+        b2, w2 = dc(r, c+half, half)
+        b3, w3 = dc(r+half, c, half)
+        b4, w4 = dc(r+half, c+half, half)
+        return (b1+b2+b3+b4, w1+w2+w3+w4)
+
+    b, w = dc(0, 0, N)
     print(w)
     print(b)
-
-
 
 if __name__ == "__main__":
     solution()
